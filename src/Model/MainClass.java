952c7a1ddb4d5c4fcc3b9fd34e032ac8c3022cd1
TheCodeMonster <75868135+thecodemonster1@users.noreply.github.com>
2022-06-28 18:44:51 +0530

    created main class

---
+++ src/Model/MainClass.java
@@ -0,0 +1,15 @@
+package Model;
+
+
+import View.LogInUI;
+
+/**
+ *
+ * @author theCodeMonster
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        LogInUI firstUI = new LogInUI();
+        firstUI.setVisible(true);
+    }
+}
