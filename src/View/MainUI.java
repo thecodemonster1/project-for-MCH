e63c204ff20e85caa86304c4fba7f62c0cb348b6
TheCodeMonster <75868135+thecodemonster1@users.noreply.github.com>
2022-06-28 18:17:17 +0530

    buttons update

--- src/View/MainUI.java
+++ src/View/MainUI.java
@@ -24,6 +24,10 @@ public class MainUI extends javax.swing.JFrame {
 
         kButton2 = new com.k33ptoo.components.KButton();
         kButton3 = new com.k33ptoo.components.KButton();
+        kButton4 = new com.k33ptoo.components.KButton();
+        kButton5 = new com.k33ptoo.components.KButton();
+        kButton6 = new com.k33ptoo.components.KButton();
+        kButton7 = new com.k33ptoo.components.KButton();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -37,7 +41,12 @@ public class MainUI extends javax.swing.JFrame {
         kButton2.setkHoverForeGround(new java.awt.Color(0, 0, 0));
         kButton2.setkHoverStartColor(new java.awt.Color(0, 255, 153));
         kButton2.setkStartColor(new java.awt.Color(102, 204, 0));
-        getContentPane().add(kButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(800, 150, 109, -1));
+        kButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                kButton2ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(kButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(640, 60, 109, -1));
 
         kButton3.setText("Invoice");
         kButton3.setFont(new java.awt.Font("Trebuchet MS", 1, 20)); // NOI18N
@@ -52,7 +61,67 @@ public class MainUI extends javax.swing.JFrame {
                 kButton3ActionPerformed(evt);
             }
         });
-        getContentPane().add(kButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(800, 230, 109, -1));
+        getContentPane().add(kButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 220, 109, -1));
+
+        kButton4.setText("Invoice");
+        kButton4.setFont(new java.awt.Font("Trebuchet MS", 1, 20)); // NOI18N
+        kButton4.setkBorderRadius(50);
+        kButton4.setkEndColor(new java.awt.Color(153, 153, 0));
+        kButton4.setkHoverEndColor(new java.awt.Color(0, 204, 204));
+        kButton4.setkHoverForeGround(new java.awt.Color(0, 0, 0));
+        kButton4.setkHoverStartColor(new java.awt.Color(0, 255, 153));
+        kButton4.setkStartColor(new java.awt.Color(102, 204, 0));
+        kButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                kButton4ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(kButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(640, 300, 109, -1));
+
+        kButton5.setText("Invoice");
+        kButton5.setFont(new java.awt.Font("Trebuchet MS", 1, 20)); // NOI18N
+        kButton5.setkBorderRadius(50);
+        kButton5.setkEndColor(new java.awt.Color(153, 153, 0));
+        kButton5.setkHoverEndColor(new java.awt.Color(0, 204, 204));
+        kButton5.setkHoverForeGround(new java.awt.Color(0, 0, 0));
+        kButton5.setkHoverStartColor(new java.awt.Color(0, 255, 153));
+        kButton5.setkStartColor(new java.awt.Color(102, 204, 0));
+        kButton5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                kButton5ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(kButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(680, 380, 109, -1));
+
+        kButton6.setText("Invoice");
+        kButton6.setFont(new java.awt.Font("Trebuchet MS", 1, 20)); // NOI18N
+        kButton6.setkBorderRadius(50);
+        kButton6.setkEndColor(new java.awt.Color(153, 153, 0));
+        kButton6.setkHoverEndColor(new java.awt.Color(0, 204, 204));
+        kButton6.setkHoverForeGround(new java.awt.Color(0, 0, 0));
+        kButton6.setkHoverStartColor(new java.awt.Color(0, 255, 153));
+        kButton6.setkStartColor(new java.awt.Color(102, 204, 0));
+        kButton6.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                kButton6ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(kButton6, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 450, 109, -1));
+
+        kButton7.setText("Invoice");
+        kButton7.setFont(new java.awt.Font("Trebuchet MS", 1, 20)); // NOI18N
+        kButton7.setkBorderRadius(50);
+        kButton7.setkEndColor(new java.awt.Color(153, 153, 0));
+        kButton7.setkHoverEndColor(new java.awt.Color(0, 204, 204));
+        kButton7.setkHoverForeGround(new java.awt.Color(0, 0, 0));
+        kButton7.setkHoverStartColor(new java.awt.Color(0, 255, 153));
+        kButton7.setkStartColor(new java.awt.Color(102, 204, 0));
+        kButton7.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                kButton7ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(kButton7, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 140, 109, -1));
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Photos/background.png"))); // NOI18N
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1280, 800));
@@ -63,6 +132,30 @@ public class MainUI extends javax.swing.JFrame {
     private void kButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_kButton3ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_kButton3ActionPerformed
+
+    private void kButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_kButton4ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_kButton4ActionPerformed
+
+    private void kButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_kButton5ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_kButton5ActionPerformed
+
+    private void kButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_kButton6ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_kButton6ActionPerformed
+
+    private void kButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_kButton7ActionPerformed
+        Invoice invc = new Invoice();
+        invc.setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_kButton7ActionPerformed
+
+    private void kButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_kButton2ActionPerformed
+        StockUI stock = new StockUI();
+        stock.setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_kButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -103,5 +196,9 @@ public class MainUI extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private com.k33ptoo.components.KButton kButton2;
     private com.k33ptoo.components.KButton kButton3;
+    private com.k33ptoo.components.KButton kButton4;
+    private com.k33ptoo.components.KButton kButton5;
+    private com.k33ptoo.components.KButton kButton6;
+    private com.k33ptoo.components.KButton kButton7;
     // End of variables declaration//GEN-END:variables
 }
